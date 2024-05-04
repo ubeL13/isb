2cbe53d235c7bd6bd863ba3e94e79ab71da59a82
UbeL13 <u.b.2004@yandex.ru>
2024-05-04 11:21:23 +0400

    task1 pep

--- lab_1/task_1/encryption.py
+++ lab_1/task_1/encryption.py
@@ -2,15 +2,33 @@ import json
 from key_for_encryption import KEY
 from constant import PATHS, SIGNS
 
-def read_config(config_file):
+
+def read_config(config_file) -> dict:
+    """
+        Load and return the configuration data from a JSON file.
+        :param config_file: The path to the JSON configuration file.
+        :return: A dictionary containing the configuration data.
+        """
     with open(config_file, "r", encoding="utf-8") as file:
         return json.load(file)
 
-def read_text(file_path):
+
+def read_text(file_path) -> str:
+    """
+        Read and return the content of a text file.
+        :param file_path: The path to the text file to be read.
+        :return: A string containing the contents of the file.
+        """
     with open(file_path, "r", encoding="utf-8") as file:
         return file.read()
 
-def polybius_encrypt(text):
+
+def polybius_encrypt(text) -> str:
+    """
+        Encrypt a text using the Polybius square cipher.
+        :param text: The plain text string to be encrypted.
+        :return: The encrypted text as a string.
+        """
     encrypted_text = ""
     for char in text.upper():
         if char in SIGNS:
@@ -20,18 +38,29 @@ def polybius_encrypt(text):
             for i, row in enumerate(KEY):
                 if char in row:
                     j = row.index(char)
-                    encrypted_text += f"{i+1}{j+1}"
+                    encrypted_text += f"{i + 1}{j + 1}"
                     found = True
                     break
             if not found:
-                encrypted_text += char  # Если символ не найден, оставляем его без изменений
+                encrypted_text += char
     return encrypted_text
 
-def write_text(file_path, text):
+
+def write_text(file_path, text) -> None:
+    """
+        Write text to a file, overwriting it if it already exists.
+        :param file_path: The path to the file where the text will be written.
+        :param text: The text to write to the file.
+        """
     with open(file_path, "w", encoding="utf-8") as file:
         file.write(text)
 
-def main():
+
+def main() -> None:
+    """
+        Main function to read configuration, encrypt the source file content,
+        and write the output to the target file.
+        """
     config = read_config(PATHS)
     folder = config["folder"]
     source_file = config["source_file"]
@@ -41,6 +70,7 @@ def main():
     encrypted_text = polybius_encrypt(source_text)
     write_text(f"{folder}/{target_file}", encrypted_text)
     print("Текст успешно зашифрован и сохранен.")
+
 
 if __name__ == "__main__":
     main()
